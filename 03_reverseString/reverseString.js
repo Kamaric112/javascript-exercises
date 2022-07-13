e378a7882fe782ea4573caa97644915be101c667
Kamaric112 <ikasuzu123@gmail.com>
2022-07-13 23:10:13 +0700

    completed first 4 lessons

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let string = ""
+    for (let i = 1 ; i <=( word.length ) ; i ++){
+    string += word[word.length -i]
+    }
+    return string
 };
 
+reverseString('hello')
 // Do not edit below this line
 module.exports = reverseString;
+
+
+//  word = "hello"  - length = 5
+//     i =1 
+//     string = word[5 -1] = word[4] = 'o'
+//     i = 2
+//     string = word [ 5 - 2 ] = word[3] = 'l'
+//     .....
+//     i = 5 
+//     string = word[5-5] = word[0]= 'h' 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+   args.forEach((arg) => {
+    const index = array.indexOf(arg) 
+    if (index >-1) {
+        array.splice(index,1)
+    }
+})
+    // return array.filter(x => !args.includes(x))
 
-};
+    return array
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
