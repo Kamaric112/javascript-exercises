63a45716ef6fc0cec0c2bee21a1268765cddf96f
Kamaric112 <ikasuzu123@gmail.com>
2022-07-24 23:05:36 +0700

    updated calculator and palindrome execises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function () {
+// const punc = /[!"#$%&'()*+,-./:;<=>?@[\]^_`/\s/g{|}~]/g;        
 
+// const palindromes = function (str) {
+    
+//     let pali =str.toLowerCase().replace(punc, "").split("") 
+
+//     let len = pali.length
+//     for (let i = 0 ; i < len/2 ; i ++){
+//         if (pali[i] !== pali[len - 1 - i]) {
+//         return false
+//         }else { return true } 
+//     }  
+// };
+
+
+const punc = /[!"#$%&'()*+,-./:;<=>?@[\]^_`/\s/g{|}~]/g;
+
+const palindromes = function (str) {
+  let pali = str.replace(punc, "").split("");
+
+  let value = false;
+  for (i = 0; i < pali.length; i++) {
+    if (pali[i] == pali[(pali.length - (i+1))]) {
+        value = true;
+    } else {
+        value = false;
+    }
+    return value;
+  }
 };
 
 // Do not edit below this line
