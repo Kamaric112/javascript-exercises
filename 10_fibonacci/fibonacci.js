088142fc6e8a81d16a91dbd4f449dc80ce4ae092
Kamaric112 <ikasuzu123@gmail.com>
2022-07-26 21:35:24 +0700

    finished all exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) { 
+    let a = parseInt(num)
+    if ( a < 0 ) {
+        return "OOPS"
+    }
+  else if (a <= 1) {
+    return a
+  } return (fibonacci(a - 1) + fibonacci(a - 2))
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
+
